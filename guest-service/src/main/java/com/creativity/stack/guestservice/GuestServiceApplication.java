e092de969865d16c283d25a7af2bfceba4ed7f05
Hansel <hansel.hir@gmail.com>
2020-12-09 11:08:05 -0500

    feat: Se implementa Eureka Server y Eureka client.

--- guest-service/src/main/java/com/creativity/stack/guestservice/GuestServiceApplication.java
+++ guest-service/src/main/java/com/creativity/stack/guestservice/GuestServiceApplication.java
@@ -2,7 +2,9 @@ package com.creativity.stack.guestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GuestServiceApplication {
 
